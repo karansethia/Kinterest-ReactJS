df07895a31e46d7a20208b0c561e117c5ea1e880
Karan <karansethia24@gmail.com>
2023-03-22 23:44:58 +0530

    Adding logo and assets for login page

--- kinterest_frontend/src/components/Login.jsx
+++ kinterest_frontend/src/components/Login.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FcGoogle } from 'react-icons/fc'
+import { GoogleLogin } from '@react-oauth/google';
+import kintLogo from './assets/kint-logo'
 
 const Login = () => {
   return (
