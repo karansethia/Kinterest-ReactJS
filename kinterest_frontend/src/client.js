a851f400611fbdd5356f861957e99b77d9386888
Karan <karansethia24@gmail.com>
2023-04-01 23:11:53 +0530

    adding security and hiding tokens and id

--- kinterest_frontend/src/client.js
+++ kinterest_frontend/src/client.js
@@ -2,10 +2,10 @@ import { createClient } from "@sanity/client";
 import ImageUrlBuilder from "@sanity/image-url"
 
 export const client = createClient({
-    projectId: 'vgaojuvy',
+    projectId: process.env.REACT_APP_SANITY_PROJECTID,
     dataset: 'production',
     apiVersion: '2021-10-21',
-    token: 'skCRNVMoFgDilgu8mydfXzvIjXulxKjd34RMzFSUeuSx8f1G6WheIwaicEqd4Ah63C3bTV22QoacfQhGt4QzS8KjZiuzd89KitlYCG5kEcHEt2fHaA5zx4aCRNQ8CmYxGSAyhAPC9wYjuAhtzpqGABvBOPH03xHlcYqCcQA6MOozrcuTPGfJ',
+    token: process.env.REACT_APP_SANITY_APITOKEN,
     useCdn: true
 })
 
