c0e89e045c27690addceb72f900c7b57f71d5c86
Karan <karansethia24@gmail.com>
2023-04-03 01:55:54 +0530

    Starting Pins component

--- kinterest_frontend/src/container/Home.jsx
+++ kinterest_frontend/src/container/Home.jsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { hiMenu } from 'react-icons/hi'
+import { AiFillCircleClose } from 'react-icons/ai'
+import { Link, Route, Routes } from 'react-router-dom'
 import { Route } from 'react-router-dom'
+
+import { Sidebar, UserProfile } from '../components'
+import { client } from '../client'
+import logo from '../assets/kint-logo'
+
+import Pins from './Pins'
 
 const Home = () => {
   return (
