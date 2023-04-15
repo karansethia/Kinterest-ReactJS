6f56434d96762f1184083069a4c79374752ce3ed
Karan <karansethia24@gmail.com>
2023-04-16 02:26:44 +0530

    Navbar styling for mobile

--- kinterest_frontend/src/container/Home.jsx
+++ kinterest_frontend/src/container/Home.jsx
@@ -16,8 +16,9 @@ import { userQuery } from '../utils/data'
 const Home = () => {
   const [ToggleSidebar, setToggleSidebar] = useState(false)
   const [user, setUser] = useState(null)
-
+  const scrollRef = useRef(null)
   
+  //! taking userdata from local storage(saved in login.jsx) and updating state of user
   const userInfo = localStorage.getItem('user')  !== undefined ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
   useEffect(() => {
     const query = userQuery(userInfo?.sub);
@@ -28,28 +29,44 @@ const Home = () => {
   }, []);
   
 
+  // 
+  useEffect(() => {
+    scrollRef.current.scrollTo(0,0)
+  }, [])
+  
+
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
       <div className="hidden md:flex h-screen flex-initial">
-        <Sidebar />
+        <Sidebar user={user && user}/>
       </div>
       <div className="flex md:hidden flex-row">
+        <div className='p-2 w-full flex flex-row justify-between items-center shadow-md bg-slate-500'>
         <img src={sideBar} className="cursor-pointer w-8 h-8" alt='user' onClick={() => setToggleSidebar(true)}/>
         <Link to='/'>
           <img src={mylogo} alt="" className='w-28'/>
         </Link>    
-        <Link to={`/user-profile/${user?._id}`}>
-          <img src={userInfo?.picture} alt="" className='w-28'/>
-        </Link>    
-      </div>
-      {ToggleSidebar && (
+        <Link to={`/user-profile/${user?.sub}`}>
+          <img src={userInfo?.picture} alt="" className='w-10'/>
+        </Link> 
+        </div>
+        {ToggleSidebar && (
         <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
           <div className='absolute w-full flex justify-end items-center p-2'>
-            <AiFillCloseCircle font-size={30} className='cursor-pointer' onClick={()=>{setToggleSidebar(false)}} />
-            <Sidebar />
+            <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={()=>{setToggleSidebar(false)}} />
           </div>
+            <Sidebar user={user && user} closeToggle={setToggleSidebar}/>
         </div>
       )}
+      </div>
+      
+      <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+        <Routes>
+          <Route path='/user-profile/:userId' element={<UserProfile />}/>
+          <Route path='/*' element={<Pins user={user && user}/>}/>
+        </Routes>
+      </div>
+
     </div>
   )
 }
