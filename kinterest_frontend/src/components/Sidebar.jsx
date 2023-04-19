79ad71ee330e40e41eb7badbadaaa66de62b4fc2
Karan <karansethia24@gmail.com>
2023-04-19 23:42:10 +0530

    sidebar ui

--- kinterest_frontend/src/components/Sidebar.jsx
+++ kinterest_frontend/src/components/Sidebar.jsx
@@ -4,11 +4,23 @@ import { NavLink, Link } from 'react-router-dom';
 
 import logo from '../assets/kint-logo.png'
 
-const Sidebar = (user, closeToggle) => {
+const Sidebar = ({user, closeToggle}) => {
+
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
+
+const categories = [
+  { name: 'Animals' },
+  { name: 'Cars' },
+  { name: 'Anime' },
+  { name: 'Nature' },
+  { name: 'Web Designs' },
+  { name: 'Other' }
+]
 
   const handleCloseSidebar = () => {
     if(closeToggle) closeToggle(false)
-  }
+  };
 
   return (
     <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
@@ -21,7 +33,24 @@ const Sidebar = (user, closeToggle) => {
           <img src={logo} alt="logo" className='w-full' />
         </Link>
         <div className="flex flex-col gap-5">
-          
+          <NavLink 
+            to='/'
+            className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+            onClick={handleCloseSidebar}
+          >
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover categories</h3>
+          {categories.slice(0, categories.length - 1).map((category) => {
+            return (<NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>)
+          })}
         </div>
       </div>
     </div>
